4294761800e0ac1dce0a3042cd3ca32f84f96379
han semaj <sangho.nah@gmail.com>
2014-08-10 22:30:30 +1200

    Define goroutines communicating via channel

--- gerri.go
+++ gerri.go
@@ -4,11 +4,9 @@ package main
 Minimal IRC bot in Go
 
 TODO:
-* store connection info in json file
-* set up goroutines for reading/writing buffers
-* declare const(ant) values for commands (e.g. NICK)
 * reply PING :cookie (e.g. PING :sinisalo.freenode.net) with PING: cookie
 * add plugins (!wik, !random, !ping, !title)
+* store connection info in json file
 */
 
 import (
@@ -17,6 +15,13 @@ import (
 	"net"
         "bufio"
         "net/textproto"
+)
+
+const (
+	USER = "USER"
+	NICK = "NICK"
+	JOIN = "JOIN"
+	SUFFIX = "\r\n"
 )
 
 func connect(server string, port string) (net.Conn, error) {
@@ -30,10 +35,34 @@ func connect(server string, port string) (net.Conn, error) {
 	return conn, err
 }
 
-func main(){
+func read(ch chan<- string, conn net.Conn) {
+	/* defines goroutine sending lines to channel */
+	reader := textproto.NewReader(bufio.NewReader(conn))
+	for {
+		line, err := reader.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+			break
+		}
+		ch <- line
+	}
+}
+
+func write(ch <-chan string, conn net.Conn) {
+	/* defines goroutine receiving lines from channel */
+	for {
+		line, ok := <-ch
+		if !ok {
+			log.Fatal("aborted: failed to receive from channel")
+			break
+		}
+		fmt.Println(line)
+	}
+}
+
+func main() {
         server, port := "chat.freenode.net", "8002"
 	nick, channel := "gerri", "#microamp"
-	msgSuffix := "\r\n"
 
         // connect to irc
 	conn, err := connect(server, port)
@@ -41,21 +70,18 @@ func main(){
 		log.Fatal(err)
 	}
 
-	// send messages; USER/NICK/JOIN
-	conn.Write([]byte("USER " + nick + " 8 * :" + nick + msgSuffix))
-	conn.Write([]byte("NICK " + nick + msgSuffix))
-	conn.Write([]byte("JOIN " + channel + msgSuffix))
+	// send messages: USER/NICK/JOIN
+	conn.Write([]byte(USER + " " + nick + " 8 * :" + nick + SUFFIX))
+	conn.Write([]byte(NICK + " " + nick + SUFFIX))
+	conn.Write([]byte(JOIN + " " + channel + SUFFIX))
 
 	defer conn.Close()
 
-        // read buffers
-	r := bufio.NewReader(conn)
-	tp := textproto.NewReader(r)
-	for {
-		line, err := tp.ReadLine()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s%s", line, msgSuffix)
-	}
+        // define goroutines communicating via channel
+	ch := make(chan string)
+	go read(ch, conn)
+	go write(ch, conn)
+
+	var input string
+	fmt.Scanln(&input)
 }
