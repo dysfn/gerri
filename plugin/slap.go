c75588345876de9dc9b7a383ae81b1a40ce04bf1
Kesara Rathnayake <kesara@kesara.lk>
2015-02-07 02:51:54 +1300

    Few enhancements to slap plugin. Issue: #6

--- plugin/slap.go
+++ plugin/slap.go
@@ -6,25 +6,42 @@ usage: !slap someone
 
 import (
 	"fmt"
-	"math/rand"
-	"strings"
 	"github.com/microamp/gerri/cmd"
 	"github.com/microamp/gerri/data"
+	"math/rand"
+	"regexp"
+	"strings"
 )
 
-func slapAction(target string) (string, error) {
-	actions := []string {
+func slapAction(target string, source_nick string, nick string) (string, error) {
+	actions := []string{
 		"slaps", "kicks", "destroys", "annihilates", "punches",
 		"roundhouse kicks", "rusty hooks", "pwns", "owns"}
-	if strings.TrimSpace(target) != "" {
+	if strings.TrimSpace(target) != "" && strings.TrimSpace(target) != nick {
 		selected_action := actions[rand.Intn(len(actions))]
 		return fmt.Sprintf("%s %s", selected_action, target), nil
+	} else {
+		selected_action := actions[rand.Intn(len(actions))]
+		return fmt.Sprintf("%s %s", selected_action, source_nick), nil
 	}
 	return "zzzzz...", nil
 }
 
+func Nick(source string) (string, error) {
+	/*
+	   Extracts nick from the IRC source data.
+	*/
+	r, err := regexp.Compile(".(?P<nick>.+)!.*")
+	if err != nil {
+		return "", err
+	}
+	return r.FindAllStringSubmatch(source, 1)[0][1], nil
+}
+
 func ReplySlap(pm data.Privmsg, config *data.Config) (string, error) {
-	slap, err := slapAction(strings.Join(pm.Message[1:], " "))
+	source_nick, err := Nick(pm.Source)
+	slap, err := slapAction(
+		strings.Join(pm.Message[1:], " "), source_nick, config.Nick)
 	if err != nil {
 		return "", err
 	}
